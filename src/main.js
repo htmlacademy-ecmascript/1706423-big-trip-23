9045745c3328645c3ff863ce94bfe4ecfbcc4225
Elena <enafukin@gmail.com>
2024-04-27 22:55:46 +0400

    Добавляет компоненты списка и точки маршрута

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import {render} from './render';
 import Filter from './view/filter';
 import Sort from './view/sort';
+import EventsList from './view/events-list';
 
 const filterContainer = document.querySelector('.trip-controls__filters');
-const sortContainer = document.querySelector('.trip-events');
+const mainContainer = document.querySelector('.trip-events');
 
 render(new Filter(), filterContainer);
-render(new Sort(), sortContainer);
+render(new Sort(), mainContainer);
+render(new EventsList(), mainContainer);
