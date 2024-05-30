7f83a1c9bbbbd8c97b49740e12ceb68b8ec6db03
Elena <enafukin@gmail.com>
2024-05-30 17:45:12 +0400

    Обучает компонент точки маршрута принимать данные

---
+++ src/utils.js
@@ -0,0 +1,32 @@
+import dayjs from 'dayjs';
+
+function getRandomArrayElement(items) {
+  return items[Math.floor(Math.random() * items.length)];
+}
+
+function getHumanDate(date, format) {
+  return dayjs(date).format(format);
+}
+
+function getTimeInterval(dateFrom, dateTo) {
+  let result = '';
+
+  const days = dayjs(dateTo).diff(dayjs(dateFrom), 'd');
+  if (days) {
+    result += days < 10 ? `0${days}D ` : `${days}D `;
+  }
+
+  const hours = dayjs(dateTo).diff(dayjs(dateFrom), 'h') % 24;
+  if (hours) {
+    result += hours < 10 ? `0${hours}H ` : `${hours}H `;
+  }
+
+  const minutes = dayjs(dateTo).diff(dayjs(dateFrom), 'm') % 60;
+  if (minutes) {
+    result += minutes < 10 ? `0${minutes}M` : `${minutes}M`;
+  }
+
+  return result;
+}
+
+export {getRandomArrayElement, getHumanDate, getTimeInterval};
