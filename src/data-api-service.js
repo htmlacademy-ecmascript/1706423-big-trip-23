d237630b750a60477f11e244612ab50cb9d2b79d
Elena <enafukin@gmail.com>
2024-06-13 22:33:19 +0400

    Реализует паттерн "Адаптер"

--- src/data-api-service.js
+++ src/data-api-service.js
@@ -18,12 +18,28 @@ export default class DataApiService extends ApiService {
     const response = this._load({
       url: 'points',
       method: Method.PUT,
-      body: point,
+      body: JSON.stringify(this.#adaptPointToServer(point)),
       headers: new Headers({'Content-Type': 'application/json'}),
     });
 
     const parsedResponse = await ApiService.parseResponse(response);
 
     return parsedResponse;
+  }
+
+  #adaptPointToServer(point) {
+    const adaptedPoint = {...point,
+      'base_price': point.basePrice,
+      'date_from': point.dateFrom instanceof Date ? point.dateFrom.toISOString() : null,
+      'date_to': point.dateTo instanceof Date ? point.dateTo.toISOString() : null,
+      'is_favorite': point.isFavorite,
+    };
+
+    delete adaptedPoint.basePrice;
+    delete adaptedPoint.dateFrom;
+    delete adaptedPoint.dateTo;
+    delete adaptedPoint.isFavorite;
+
+    return adaptedPoint;
   }
 }
--- src/model/points-model.js
+++ src/model/points-model.js
@@ -16,7 +16,7 @@ export default class PointsModel extends Observable {
     this.#dataApiService = dataApiService;
 
     this.#dataApiService.points.then((points) => {
-      console.log(points);
+      console.log(points.map(this.#adaptPointToClient));
     });
   }
 
@@ -70,5 +70,21 @@ export default class PointsModel extends Observable {
     ];
 
     this._notify(updateType);
+  }
+
+  #adaptPointToClient(point) {
+    const adaptedPoint = {...point,
+      basePrice: point['base_price'],
+      dateFrom: point['date_from'] !== null ? new Date(point['date_from']) : point['date_from'],
+      dateTo: point['date_to'] !== null ? new Date(point['date_to']) : point['date_to'],
+      isFavorite: point['is_favorite'],
+    };
+
+    delete adaptedPoint['base_price'];
+    delete adaptedPoint['date_from'];
+    delete adaptedPoint['date_to'];
+    delete adaptedPoint['is_favorite'];
+
+    return adaptedPoint;
   }
 }
