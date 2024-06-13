d5a7a96fdeab421a0b3f9fcda48eb992d42f6a59
Elena <enafukin@gmail.com>
2024-06-13 16:26:42 +0400

    Ограничивает поле ввода цены только числами

--- src/view/event-edit-form.js
+++ src/view/event-edit-form.js
@@ -97,7 +97,7 @@ const createEventEditFormTemplate = (point, options, places) => {
               <span class="visually-hidden">Price</span>
               &euro;
             </label>
-            <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${basePrice ?? ''}">
+            <input class="event__input  event__input--price" id="event-price-1" type="number" name="event-price" value="${basePrice ?? 0}">
           </div>
 
           <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
